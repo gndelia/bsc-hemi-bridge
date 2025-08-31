04d96d8cd95b3f4ada76d48af5cddbbb20cc1919
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2025-08-31 09:49:55 -0300

    Add link to source code

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -1,9 +1,23 @@
 import { Bridge } from "components/bridge";
+import { HeartIcon } from "components/icons/heart";
 
 export const App = () => (
-  <div>
-    <main className="flex h-screen items-center justify-center">
+  <div className="flex h-screen flex-col">
+    <main className="my-auto justify-self-center">
       <Bridge />
     </main>
+    <footer className="justify-self-end pb-4">
+      <p className="flex flex-wrap items-center justify-center gap-x-1 text-center text-sm text-neutral-500">
+        Made with <HeartIcon /> by Gonzalo. Source code on{" "}
+        <a
+          className="text-orange-500 hover:text-orange-700"
+          href="https://github.com/gndelia/bsc-hemi-bridge"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </p>
+    </footer>
   </div>
 );
